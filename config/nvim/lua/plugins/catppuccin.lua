4504ab4f92666e64faa3b30ec0d3c711c0aa4281
realamritthapa <realamritthapa@gmail.com>
2025-07-04 15:36:52 -0700

    added transparent background and tmux changes

--- config/nvim/lua/plugins/catppuccin.lua
+++ config/nvim/lua/plugins/catppuccin.lua
@@ -6,6 +6,9 @@
     name = "catppuccin",
     priority = 1000,
     config = function()
+      require("catppuccin").setup({
+        transparent_background = true,
+      })
       vim.cmd.colorscheme "catppuccin-mocha"
     end
   }
