94033920986b013850fc37045962acdc5834b28f
realamritthapa <realamritthapa@gmail.com>
2025-07-04 17:19:28 -0700

    font change, edit for showing errors

--- config/nvim/lua/plugins/lsp-config.lua
+++ config/nvim/lua/plugins/lsp-config.lua
@@ -18,13 +18,33 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
     local lspconfig = require("lspconfig")
-    lspconfig.lua_ls.setup({})
-    lspconfig.ts_ls.setup({})
-    lspconfig.html.setup({})
-    lspconfig.cssls.setup({})
-    lspconfig.tailwindcss.setup({})
+    --lspconfig.lua_ls.setup({})
+    --lspconfig.ts_ls.setup({})
+    --lspconfig.html.setup({})
+    --lspconfig.cssls.setup({})
+    --lspconfig.tailwindcss.setup({})
     vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
     vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>e", function()
+        vim.diagnostic.open_float(nil, { focus = false })
+      end, opts)
+
+      -- Diagnostics config
+      vim.diagnostic.config({
+        virtual_text = false,
+        signs = true,
+        underline = true,
+        update_in_insert = false,
+        severity_sort = true,
+      })
+
+      -- Show diagnostics on hover (like VSCode)
+      vim.o.updatetime = 300
+      vim.api.nvim_create_autocmd("CursorHold", {
+        callback = function()
+          vim.diagnostic.open_float(nil, { focus = false })
+        end
+      })
     end
   }
 }
--- config/nvim/lua/vim-options.lua
+++ config/nvim/lua/vim-options.lua
@@ -1,7 +1,23 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.cmd('set number')
-vim.cmd('set relativenumber')
+--vim.o.clipboard = "unnamedplus"
+--vim.cmd("set expandtab")
+--vim.cmd("set tabstop=2")
+--vim.cmd("set softtabstop=2")
+--vim.cmd("set shiftwidth=2")
+--vim.cmd('set number')
+--vim.cmd('set relativenumber')
+--vim.g.mapleader = " "
+-- System clipboard integration
+vim.o.clipboard = "unnamedplus"
+
+-- Indentation and tab settings
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+
+-- Line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Leader key
 vim.g.mapleader = " "
