21ad13896f5154d49415ea6a584683bfd9761aa5
realamritthapa <realamritthapa@gmail.com>
2025-09-15 15:21:51 -0700

    updated ghostty theme
    disable copilot for neetcode dir

--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -6,5 +6,18 @@ return{
       expr = true,
       replace_keycodes = false
     })
+
+      vim.api.nvim_create_autocmd({ "BufEnter", "BufReadPost", "BufNewFile" }, {
+      callback = function()
+        local path = vim.api.nvim_buf_get_name(0)  -- full file path
+        if path:find("^/Users/amrit/neetcode") then
+          vim.b.copilot_enabled = false
+          vim.cmd("Copilot disable")
+        else
+          vim.b.copilot_enabled = true
+          vim.cmd("Copilot enable")
+        end
+      end,
+    })
   end,
 }
--- config/nvim/lua/plugins/lsp-config.lua
+++ config/nvim/lua/plugins/lsp-config.lua
@@ -10,7 +10,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require ("mason-lspconfig").setup({
-        ensure_installed = {"lua_ls", "ts_ls", "html", "cssls", "tailwindcss", "gopls", "terraformls"},
+        ensure_installed = {"lua_ls", "ts_ls", "html", "cssls", "tailwindcss", "gopls", "terraformls", "pylsp"},
       })
     end
   },
